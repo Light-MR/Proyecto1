627759301bfb6ea7028185b5e28ab68251ef8a16
julietavg <74714022+julietavg@users.noreply.github.com>
2021-11-06 17:46:46 -0600

    Update Card.java

--- src/fciencias/edatos/oldMaid/Card.java
+++ src/fciencias/edatos/oldMaid/Card.java
@@ -1,5 +1,39 @@
-import reference.*;
-public class Card {
+import reference.*;    
+  public class Card {
+
+	String cardName;
+	String cardSign;
+	int pairId;
+
+	public Card(){
+		
+	}
+	public Card(String cardName, String cardSign, int pairId){
+		this.cardName = cardName;
+		this.cardSign = cardSign;
+		this.pairId   = pairId;
+	}
+
+	public String getCardName() {
+		return cardName;
+	}
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	public String getCardSign() {
+		return cardSign;
+	}
+	public void setCardSign(String cardSign) {
+		this.cardSign = cardSign;
+	}
+	public int getPairId() {
+		return pairId;
+	}
+
+	public void setPairId(int pairId) {
+		this.pairId = pairId;
+	}
+}
     
     public static void main(String[] args) {
         

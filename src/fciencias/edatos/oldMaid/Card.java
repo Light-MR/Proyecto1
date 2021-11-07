ac247e3de43069d409bf1d164962db1cbe967496
julietavg <74714022+julietavg@users.noreply.github.com>
2021-11-06 22:24:45 -0600

    Documentado

--- src/fciencias/edatos/oldMaid/Card.java
+++ src/fciencias/edatos/oldMaid/Card.java
@@ -1,41 +1,93 @@
-import reference.*;    
+import reference.*;  
+
+/**
+ * Clase que implementa las cartas
+ * @author Julieta Vargas Gutiérrez 318341945
+ * @author
+ * @version 1.0 Noviembre 2021.
+ * @since Estructuras de datos 2022-1.
+ */
+
+
   public class Card {
+      
+    //Atributos
+	String nombreCarta;
+	String signoCarta;
+	int par;
+       	
+        /***********************************************
+        **           CONSTRUCTORES                    **
+        ************************************************/
+        public Card(){
+            }
+        /**
+	* Crea una nueva varta
+	* @param nombreCarta el nombre de la carta.
+	* @param signoCarta el signo de la carta.
+	* @param par si son pares.
+	*/
+	
+	public Card(String nombreCarta, String signoCarta, int par){
+		this.nombreCarta = nombreCarta;
+		this.signoCarta = signoCarta;
+		this.par = par;
+	}
+         /***********************************************
+        **         MÉTODOS DE ACCESO                  **
+        ************************************************/
 
-	String cardName;
-	String cardSign;
-	int pairId;
-
-	public Card(){
-		
-	}
-	public Card(String cardName, String cardSign, int pairId){
-		this.cardName = cardName;
-		this.cardSign = cardSign;
-		this.pairId   = pairId;
-	}
-
-	public String getCardName() {
-		return cardName;
-	}
-	public void setCardName(String cardName) {
-		this.cardName = cardName;
-	}
-	public String getCardSign() {
-		return cardSign;
-	}
-	public void setCardSign(String cardSign) {
-		this.cardSign = cardSign;
-	}
-	public int getPairId() {
-		return pairId;
-	}
-
-	public void setPairId(int pairId) {
-		this.pairId = pairId;
-	}
-}
-    
-    public static void main(String[] args) {
+       /**
+	* Regresa el nombre de la carta
+	* @return el nombre de la carta
+	*/
         
-    }
+	public String getNombreCarta() {
+		return nombreCarta;
+	}
+        
+        /**
+	* Modifca el nombre de la carta
+	* @param nombreCarta el nuevo nombre de la carta
+	*/
+            
+	public void setNombreCarta(String nombreCarta) {
+		this.nombreCarta = nombreCarta;
+	}
+        
+        /**
+	* Regresa el signo de la carta
+	* @return el signo de la carta
+	*/
+        
+	public String getSignoCarta() {
+		return signoCarta;
+	}
+        
+         /**
+	* Modifca el signo de la carta
+	* @param signoCarta el nuevo nombre de la carta
+	*/
+        
+	public void setSignoCarta(String signoCarta) {
+		this.signoCarta = signoCarta;
+	}
+        
+        /**
+	* Regresa si la carta es par
+	* @return es par
+	*/
+        
+	public int getPar() {
+		return par;
+	}
+        
+        /**
+	* Modifca si la carta es par
+	* @param par si la carta es par
+	*/
+
+	public void setPar(int par) {
+		this.par = par;
+	}
 }

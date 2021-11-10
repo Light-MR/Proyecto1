31c5e9cc16577c8904ae4a8c45dbc8104aaa76a4
julietavg <74714022+julietavg@users.noreply.github.com>
2021-11-10 02:39:20 -0600

    Update Card.java

--- src/fciencias/edatos/oldMaid/Card.java
+++ src/fciencias/edatos/oldMaid/Card.java
@@ -1,9 +1,8 @@
 import reference.*;  
-
 /**
  * Clase que implementa las cartas
  * @author Julieta Vargas Gutiérrez 318341945
- * @author
+ * @author Reyes Ramos Luz María 318211073
  * @version 1.0 Noviembre 2021.
  * @since Estructuras de datos 2022-1.
  */
@@ -12,17 +11,38 @@ import reference.*;
   public class Card {
       
     //Atributos
+
 	private String nombreCarta;
-	private String signoCarta;
 
-	private int par;
+	/** Card symbol : "♠", "♦", "♥", "♣" */
+	private String suit;
 
-	/** Posibles pares de una carta */ //Dado a que una carta tiene 2 pares
-	int[] pairs;
+	/**Card values: 1= As, 2, 3,...,13 */
+	private int value;
 
+	
 	/**Representación de la carta */
 	private String cardDraw;
        	
+
+
+
+	 //Card color
+	 private final static String red="\033[31m"; 
+	 private final static String blue="\033[34m"; 
+	 private final static String reset="\u001B[0m";
+ 
+	 private final static String green="\033[32m"; 
+	 private final static String yellow="\033[33m"; 
+	 private final static String purple="\033[35m"; 
+	 private final static String cyan="\033[36m"; 
+	 private final static String white = "\u001B[37m";
+
+	
+	/**Representación de la cara trasera de una carta */
+	private final  String  backCard = white+"🂠"+reset;
+
+
         /***********************************************
         **           CONSTRUCTORES                    **
         ************************************************/
@@ -32,14 +52,13 @@ import reference.*;
     /**
 	* Crea una nueva varta
 	* @param nombreCarta el nombre de la carta.
-	* @param signoCarta el signo de la carta.
+	* @param suit el signo de la carta.
 	* @param par si son pares.
 	*/
 	
-	public Card(String nombreCarta, String signoCarta, int par){
+	public Card(String nombreCarta, String suit){
 		this.nombreCarta = nombreCarta;
-		this.signoCarta = signoCarta;
-		this.par = par;
+		this.suit = suit;
 	}
 
          /***********************************************
@@ -69,54 +88,134 @@ import reference.*;
 	* @return el signo de la carta
 	*/
         
-	public String getSignoCarta() {
-		return signoCarta;
+	public String getSuit() {
+		return suit;
 	}
         
-         /**
+     /**
 	* Modifca el signo de la carta
 	* @param signoCarta el nuevo nombre de la carta
 	*/
         
-	public void setSignoCarta(String signoCarta) {
-		this.signoCarta = signoCarta;
+	public void setSuit(String signoCarta) {
+		suit = signoCarta;
+	}
+        
+    /**
+	* Obtiene el valor de la carta: "1 = As. 2, , 3..."
+	* @return  valor de la carta
+	*/
+        
+	public int getValue() {
+		return value;
 	}
         
         /**
-	* Regresa si la carta es par
-	* @return es par
-	*/
-        
-	public int getPar() {
-		return par;
-	}
-        
-        /**
-	* Modifca si la carta es par
-	* @param par si la carta es par
+	* Modifca el valor de la carta
+	* @param value el nuevo valor de la carta
 	*/
 
-	public void setPar(int par) {
-		this.par = par;
+	public void setValue(int value) {
+		this.value = value;
 	}
 
 
+	/** Obtiene la epresentacion de la cara de una carta
+	 * @return cara de la carta.
+	 */
 	public String getDrawCard(){
 		return cardDraw;
 	}
 
+	/** Modifica la representación de la cara de una carta
+	 * @param draw cara de la carta.
+	 */
 	public void setDraw(String draw){
 		this.cardDraw = draw;
 	}
-	public int[] getPairs(){
-		return pairs;
+	
+	/**Obtiene la cara trasera de la carta 
+	 * @return cara trasera de la carta
+	*/
+	public String getBack(){
+		return backCard;
 	}
-	public void setPairs(int[] pairs){
-		this.pairs = pairs;
-	}
+
+	
 
 	@Override 
 	public String toString(){
-		return  cardDraw + "\t["+getNombreCarta() + "  "+ getSignoCarta() + "]\n";
+		return  "\n\t"+cardDraw + "\t["+getNombreCarta() + "  "+ getSuit() + "]"+"";
 	}
+
+	/**
+	 * Asigna valor a una carta dada:
+	 * Card values: 1= As, 2, 3,...,10, 11=Jack, 12 =Queen, 13 = King.
+	 * @param card La carta a asignar valor.
+	 */
+	public void auxAsinngValue(Card card){
+		String name = card.getNombreCarta();
+		switch (name) {
+			case "Ace":
+				card.setValue(1);
+				card.setDraw(red+card.getDrawCard()+ reset);
+				break;
+			case "Two":
+			    card.setValue(2);
+				card.setDraw(blue+card.getDrawCard()+ reset);
+				break;
+			case "Three":
+			    card.setValue(3);
+				card.setDraw(green+card.getDrawCard()+ reset);
+				break;
+			case "Four":
+			    card.setValue(4);
+				card.setDraw(purple+card.getDrawCard()+ reset);
+				break;
+			case "Five":
+			    card.setValue(5);
+				card.setDraw(cyan+card.getDrawCard()+ reset);
+				break;
+			case "Six":
+			    card.setValue(6);
+				card.setDraw(yellow+card.getDrawCard()+ reset);
+				break;
+			case "Seven":
+			    card.setValue(7);
+				card.setDraw(blue+card.getDrawCard()+ reset);
+				break;
+			case "Eight":
+			    card.setValue(8);
+				card.setDraw(red+card.getDrawCard()+ reset);
+				break;
+			case "Nine":
+			    card.setValue(9);
+				card.setDraw(purple+card.getDrawCard()+ reset);
+				break;
+			case "Ten":
+			    card.setValue(10);
+				card.setDraw(green+card.getDrawCard()+ reset);
+				break;
+			case "Jack":
+			    card.setValue(11);
+				card.setDraw(yellow+card.getDrawCard()+ reset);
+				break;
+			case "Queen":
+			    card.setValue(12);
+				card.setDraw(cyan+card.getDrawCard()+ reset);
+				break;
+			case "King":
+			    card.setValue(13);
+				card.setDraw(yellow+card.getDrawCard()+ reset);
+				break;
+			default:
+                System.out.println("Algo salió mal (;._,)");
+				break;
+		}
+	}
+
+	
+
+
+
 }

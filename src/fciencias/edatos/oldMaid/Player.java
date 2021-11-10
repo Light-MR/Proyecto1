9ac75b378207a692848f055842bc8fe1757b2ebb
julietavg <74714022+julietavg@users.noreply.github.com>
2021-11-10 02:41:43 -0600

    Update Player.java

--- src/fciencias/edatos/oldMaid/Player.java
+++ src/fciencias/edatos/oldMaid/Player.java
@@ -1,77 +1,137 @@
-import reference.*;  
+import reference.*;
+
 /**
- * Clase que implementa el jugador
+ * Clase que implementa el mazo
  * @author Julieta Vargas Gutiérrez 318341945
- * @author
+ * @author Reyes Ramos Luz Marìa  318211073
  * @version 1.0 Noviembre 2021.
  * @since Estructuras de datos 2022-1.
  */
 
-public class Player {
-        Deck deck;      
-   
-     //Atributos   
-	String name;
- 
-        /***********************************************
-        **           CONSTRUCTORES                    **
-        ************************************************/
+
+public class Deck {
+
+     /** Listas doblemente ligadas para cartas */ //
+	DoubleLinkedList<Card> decks;
+
+     /**Constuctor a partir de listas doblemente ligadas*/
+	public Deck(){
+		this.decks = new DoubleLinkedList<>();
+	}
+
+	public Deck(DoubleLinkedList<Card> cards){
+		this.decks = cards;
+	}
+
+
+        /**
+	* Permite añadir carta
+        * @param card la carta a agregar
+	*/
+
+	public void addToDeck (Card card){
+		this.decks.add(getSize(),card);
+	}
+
+    /**
+	* Permite remover una carta
+    * @param i indice de la carta que quieres remover
+	*/
+
+       public Card removeFromDeck (int i){
+		return this.decks.remove(i);
+	}
+
+    /**
+	* Regresa el tamaño de la baraja
+	* @return el tamaño de la baraja
+	*/
+	public int getSize (){
+		return decks.size();
+	}
       
+    
+       
+
 	/**
-	* Crea una nuevo jugador apartir del mazo
+	* Regresa apartir de listas doblemente ligadas un mazo con cartas
+	* @return decks
 	*/
-	public Player(){
-	      this.deck = new Deck();
+	public DoubleLinkedList<Card> getDecks() {
+		return decks;
 	}
-        
+
+    /**
+	* Modifca el mazo apartir de una lista de cartas.
+    * @param decks Lista de cartas.
+	*/
+	public void setDecks(DoubleLinkedList<Card> decks) {
+		this.decks = decks;
+	}
+
 	/**
-	* Crea una nuevo jugador
-	* @param name nombre del jugador.
-	*/
-        
-	public Player(String name){
-		this.name = name + " ٩(●̮̮̃•̃)۶";
-		this.deck = new Deck();
-	}
-        
-        
-        /***********************************************
-        **         MÉTODOS DE ACCESO                  **
-        ************************************************/
-        
-        /**
-	* Regresa el mazo
-	* @return deck.
-	*/
-        
-	public Deck getDeck() {
-		return deck;
-	}
-        
-        /**
-	* Modifica el mazo 
-	* @param deck el nuevo nombre de la mascota.
-	*/
+	 * Determina si en una lista de cartas hay elementos duplicados, es decir
+	 * determina si existe un par de cartas iguales en valor.
+	 * @return true si hay par, false en otro caso.
+	 */
+	public boolean duplicatedCards(){
+		int value1 = 0, value2 = 0;
+		for(int n = 0; n<getSize()-1; n++){
+			for(int k = n+1; k<getSize(); k++){
+				value1 = getDecks().get(n).getValue();
+				value2 = getDecks().get(k).getValue();
+				if(value1 == value2)
+					return true;
 
-	public void setDeck(Deck deck) {
-		this.deck = deck;
+			}
+		}
+		return false;
 	}
-        
-         /**
-	* Regresa el nombre del jugador
-	* @return name.
-	*/
 
-	public String getName() {
-		return name;
-	}
-        
-        /**
-	* Modifica el nombre del jugador
-	* @param name.
-	*/
+	/**
+	 * Remueve los pares de una lista de cartas (si los hay) version.
+	 * @return Queue con los elementos eliminados
+	 */
+	public Queue<Card> discardPairs(String playerNam){
+		Queue<Card> removed = new Queue<>();
+		if(!duplicatedCards()){
+			return removed;
+		}
+		try {
+			DoubleLinkedList<Card> cards = getDecks();
+			int value = 0, value1=0;
+				Card card1 = new Card(), card2 = new Card();
+				System.out.print("\n\tCartas eliminadas de "+playerNam +":");
+				for(int n = 0; n<getSize(); n++){
+					for(int k=n+1; k<getSize(); k++){
+						value = cards.get(n).getValue();
+						value1 = cards.get(k).getValue();
+						if(value == value1){
+							card1= cards.remove(n);
+							card2 = cards.remove(k-1);
+							removed.enqueue(card1);
+							removed.enqueue(card2);
+							n=0;
+							k= n+1;
+							System.out.print("\t"+card1 + "\n\t"+ card2+"\n\n");
+						}
+					}
+				}
 
-	public void setName(String name) {
-		this.name = name;
+		} catch (IndexOutOfBoundsException | NullPointerException e) {
+			System.out.println("\n\tSomething went wrong! D:");
+			return removed;
+		}
+		return removed; //será de utilidad para el historial
 	}
+
+
+
+	@Override
+	public String toString(){
+		return getDecks().toString();
+	}
+
+
+
 }
